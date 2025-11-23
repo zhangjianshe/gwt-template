b67ecde4a362120b2bbcece8a0c252445f7c5b72
zhangjianshe <zhangjianshe@gmail.com>
2025-11-23 22:03:04 +0800

    添加软件模块

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t hub.cangling.cn/cangling/gwt-template:1.0 .
-docker pushd hub.cangling.cn/cangling/gwt-template:1.0
+docker push hub.cangling.cn/cangling/gwt-template:1.0
