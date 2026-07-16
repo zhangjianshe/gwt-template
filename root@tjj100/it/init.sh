f2c52751c4a0b65b35d73607c9f612b3d5c76f22
zhangjianshe <zhangjianshe@gmail.com>
2026-07-17 06:13:05 +0800

    add it

---
+++ root@tjj100/it/init.sh
@@ -0,0 +1,8 @@
+$PWD=$(pwd)
+mkdir -p "$PWD/data/step-ca-data/certs"
+sudo chown -R 1000:1000 "$PWD/data/step-ca-data"
+docker run --rm -it -v "$PWD/data/step-ca-data:/home/step" hub.cangling.cn/docker/smallstep/step-ca:latest step ca init
+
+echo QAZwsx@1234 | sudo tee "$PWD/data/step-ca-data/secrets/password"
+sudo chown -R 1000:1000 "$PWD/data/step-ca-data/secrets/password"
+
